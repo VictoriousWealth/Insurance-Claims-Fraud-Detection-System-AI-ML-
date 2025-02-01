a676b63c87daf1da67c67f086818d64be75d7359
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-02-01 01:51:49 +0000

    Dataset obtained and readable

--- backend/app.py
+++ backend/app.py
@@ -4,16 +4,11 @@ import numpy as np
 import random
 from sklearn import datasets as da 
 import kagglehub
-import shutil
 
-source = r"C:\Users\efeon\.cache\kagglehub\datasets\mastmustu\insurance-claims-fraud-data\versions\2\employee_data.csv"  # Source file path
-destination = r"C:\Users\efeon\Insurance Claims Fraud Detection System\backend\employee_data.csv"  # Destination directory
-shutil.move(source, destination)
+employee_dataset = pandas.read_csv("./resources/employee_data.csv")
+insurance_dataset = pandas.read_csv("./resources/insurance_data.csv")
+vendor_dataset = pandas.read_csv("./resources/vendor_data.csv")
 
-source = r"C:\Users\efeon\.cache\kagglehub\datasets\mastmustu\insurance-claims-fraud-data\versions\2\insurance_data.csv"  # Source file path
-destination = r"C:\Users\efeon\Insurance Claims Fraud Detection System\backend\insurance_data.csv"  # Destination directory
-shutil.move(source, destination)
-
-source = r"C:\Users\efeon\.cache\kagglehub\datasets\mastmustu\insurance-claims-fraud-data\versions\2\vendor_data.csv"  # Source file path
-destination = r"C:\Users\efeon\Insurance Claims Fraud Detection System\backend\vendor_data.csv"  # Destination directory
-shutil.move(source, destination)
+print("Employee dataset shape: ", employee_dataset.shape)
+print("Insurance dataset shape: ", insurance_dataset.shape)
+print("Vendor dataset shape: ", vendor_dataset.shape)
